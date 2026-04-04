9a711b15be356a29bc2742e57b3e04dbcdfdbc65
henrydvies <85992819+henrydvies@users.noreply.github.com>
2026-04-04 21:37:46 +0100

    Add: Schema for validation + clean up of othe rbits

--- src/app/schemas/eval.py
+++ src/app/schemas/eval.py
@@ -2,10 +2,5 @@ from pydantic import BaseModel
 from typing import Dict, Any, Optional
 
 class EvalResponse(BaseModel):
-    """Evaluation response schema.
-
-    Args:
-        BaseModel : Pydantic BaseModel for data validation.
-    """
     ticker: str
     metrics: Dict[str, Dict[str, Optional[float]]]
