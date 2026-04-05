dd9aefb634658b29885a8dd78fce0f79e5ea8b7d
henrydvies <85992819+henrydvies@users.noreply.github.com>
2026-04-05 19:52:42 +0100

    Feat: Define schema for the sentiment reddit

---
+++ src/app/schemas/sentiment.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from enum import Enum
+
+# Enum for the status of sentiment analysis based of data availability and confidence.
+class SentimentStatus(str, Enum):
+    OK = "OK"
+    LOW_DATA = "LOW_DATA"
+    NOISY = "NOISY"
+    AMBIGUOUS_ENTITY = "AMBIGUOUS_ENTITY"
+    STALE = "STALE"
+    SOURCE_ERROR = "SOURCE_ERROR"
+
+class SentimentResponse(BaseModel):
+    ## Reddit Based
+    reddit_score_7d: float = Field(..., description="Average sentiment score from Reddit posts mentioning the stock over the past 7 days.")
+    reddit_score_30d: float = Field(..., description="Average sentiment score from Reddit posts mentioning the stock over the past 30 days.")
+    reddit_confidence_7d: float = Field(..., description="Confidence level of the Reddit sentiment score over the past 7 days.")
+    reddit_confidence_30d: float = Field(..., description="Confidence level of the Reddit sentiment score over the past 30 days.")
+    reddit_mentions_7d: int = Field(..., description="Number of mentions of the stock on Reddit over the past 7 days.")
+    reddit_mentions_30d: int = Field(..., description="Number of mentions of the stock on Reddit over the past 30 days.")
+    reddit_sources_7d: int = Field(..., description="Number of unique Reddit sources mentioning the stock over the past 7 days.")
+    reddit_sources_30d: int = Field(..., description="Number of unique Reddit sources mentioning the stock over the past 30 days.")
+    reddit_status_7d: SentimentStatus = Field(..., description="Status of the Reddit sentiment analysis over the past 7 days.")
+    reddit_status_30d: SentimentStatus = Field(..., description="Status of the Reddit sentiment analysis over the past 30 days.")
+    
+    ## Other sources
